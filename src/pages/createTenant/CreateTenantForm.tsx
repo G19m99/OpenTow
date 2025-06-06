8dc38a8df92ad4359894b2cf42ad572ef275cb9f
G19m99 <gershymenzer@gmail.com>
2025-06-06 00:40:05 -0400

    Bug Fix fixed placeholder text for email

--- src/pages/createTenant/CreateTenantForm.tsx
+++ src/pages/createTenant/CreateTenantForm.tsx
@@ -96,7 +96,7 @@ const CreateTenantForm = () => {
                 name="email"
                 type="email"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Brief description of your organization"
+                placeholder="Enter contact email for your organization"
               />
             </div>
 
